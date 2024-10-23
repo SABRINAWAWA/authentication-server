f3c86555c9568b7b5ac361f23708379851fcea94
SABRINAWAWA <sabrinawang4835@gmail.com>
2024-10-23 16:56:17 -0700

    Initial commit

---
+++ src/test/java/AuthenticationServer/authentication_server/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package AuthenticationServer.authentication_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
